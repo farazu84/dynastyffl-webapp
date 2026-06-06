5d8d8904b5b15e6a6b581bfa02e7c9aa4ae51ec0
Faraz Ansari <67295001+farazu84@users.noreply.github.com>
2026-06-05 17:48:43 -0700

    fix evil global styling

--- src/views/team/Team.js
+++ src/views/team/Team.js
@@ -99,7 +99,7 @@ const Team = React.memo(() => {
     }
 
     return (
-        <main>
+        <main className="team-page">
             <TeamHeader team={pickedTeam} />
             <NewsBar articles={articles} />
             <div className="team-content-split">
